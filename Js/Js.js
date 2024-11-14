398f1784bc4bb0979bd6dbf7b8deeda39adc5479
VoxySc <TarikJarovicSc@gmail.com>
2024-11-14 13:55:28 +0100

    Add files via upload
    0.0.6

---
+++ Js/Js.js
@@ -0,0 +1,23 @@
+// Buttons Start 
+function Home() {
+    window.location.href = 'https://Voxy5c.github.io/';
+}
+function Login() {
+    window.location.href = 'https://Voxy5c.github.io/Login.html'
+}
+function Sign_Up() {
+    window.location.href = 'https://Voxy5c.github.io/Sign_Up.html'
+}
+function myf() {
+    window.open("https://youtube.com/@Voxy72"); 
+    alert("Hi from Dragan")
+    }
+    
+function mys() { 
+    window.open("https://github.com/Voxy72");
+     alert("Zuri8")
+    }
+function donke() {
+    window.open('https://Voxy5c.github.io/', '_blank');
+    }
+// Buttons End
